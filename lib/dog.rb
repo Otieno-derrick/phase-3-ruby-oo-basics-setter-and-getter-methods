b7b0062f4452901eeadf7dc3e41f83793bd53ad2
otieno-derrick <otieno.derrrick@student.moringaschool.com>
2022-08-20 21:23:36 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,20 @@
 class Dog
-  attr_accessor :name, :breed
+
 
     def name=(name)
         @name = name
     end
 
-    # def name
-    #     @name
-    # end
+    def name
+        @name
+    end
 
     def breed=(breed)
         @breed = breed
     end
 
-    # def breed
-    #     @breed
-    # end
+    def breed
+        @breed
+    end
 
 end
